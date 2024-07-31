1f1d54f2752cba5af18d8706f35cd08ede908948
gerardjr42 <ggarciajr12@gmail.com>
2024-07-30 21:54:55 -0400

    add validations

---
+++ back-end/validations/checkSongs.js
@@ -0,0 +1,41 @@
+//Validation to creting name
+const checkName = (req, res, next) => {
+  if (req.body.name) {
+    return next();
+  } else {
+    res.status(400).json({error: "Name is required"});
+  }
+};
+
+//Validation for creating is_favorite
+const checkBoolean = (req, res, next) => {
+  const { is_favorite } = req.body;
+  if(
+    is_favorite == "true" ||
+    is_favorite == "false" ||
+    is_favorite == "undefined" ||
+    typeof is_favorite == "boolean"
+  ) {
+    next();
+  }else {
+    res.status(400).json({error: "is_favorite must be a boolean value"});
+  }
+};
+
+// Make sure that the URL starts with http:// or https://
+const validateURL = (req, res, next) => {
+  if (req.body.url) {
+    if (
+      req.body.url.substring(0, 7) === "http://" ||
+      req.body.url.substring(0, 8) === "https://"
+    ) {
+      return next();
+    } else {
+      return res.status(400).json({ error: `You forgot to start your url with http:// or https://` });
+    }
+  } else {
+    return next();
+  }
+};
+
+module.exports = { checkName, checkBoolean, validateURL};

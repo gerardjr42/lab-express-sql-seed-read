340142d1929156ee626de924ec7e863a63c45c25
gerardjr42 <ggarciajr12@gmail.com>
2024-07-30 21:19:02 -0400

    update seed.sql: add 2 more songs to database

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,4 +2,6 @@
 
 INSERT INTO songs (name, artist, album, time, is_favorite)
 VALUES
-('Chihiro', 'Billie Eilish', 'HIT ME HARD AND SOFT', '5:03', true)
+('Chihiro', 'Billie Eilish', 'HIT ME HARD AND SOFT', '5:03', true),
+('Where''s My Love(Acoustic Cover)', 'SYML', 'SYML', '4:12', true),
+('Anchor', 'Novo Amor', 'Bathing Beach', '4:16', true);

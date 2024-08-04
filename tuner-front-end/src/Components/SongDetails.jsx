19f66b7154732d1648d4a5df47bc67b150c337ec
gerardjr42 <ggarciajr12@gmail.com>
2024-08-04 16:16:40 -0400

    create edit and delete functionality

--- tuner-front-end/src/Components/SongDetails.jsx
+++ tuner-front-end/src/Components/SongDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 // import API
 const API = import.meta.env.VITE_API_URL;
@@ -39,10 +39,28 @@ export default function SongDetails() {
     <>
       {/* Add Song Details here, with edit button and delete button */}
       <div>
-        <h1>{song.name}</h1>
-        <h2>{song.artist}</h2>
-        <p>{song.album}</p>
+        <h1>Name: {song.name}</h1>
+        <h2>Artist: {song.artist}</h2>
+        <p>Album: {song.album}</p>
+        <p>Time: {song.time}</p>
+        <p>Favorite: {song.is_favorite ? "✅" : "❌"}</p>
+        <div>
+          <Link to={`/songs/${id}/edit`}>
+            <button className="mr-2 rounded-md border bg-blue-300 px-2">
+              Edit
+            </button>
+          </Link>
+          <Link>
+            <button
+              onClick={handleDelete}
+              className="rounded-md border bg-blue-300 px-2"
+            >
+              Delete
+            </button>
+          </Link>
+        </div>
       </div>
+      {/* Add edit and delete button */}
     </>
   );
 }
